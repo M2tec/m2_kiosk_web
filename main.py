8bc72b19eb2151054784eb12751e3ebaf85478e5
Maarten Menheere <m.menheere@m2tec.nl>
2023-01-03 11:47:08 +0100

    Fix whitespace with autopep8

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import os 
+import os
 
 from flask import Flask, render_template, request
 from flask_cors import CORS
@@ -14,7 +14,7 @@ CORS(app)
 
 logging.getLogger('flask_cors').level = logging.DEBUG
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) # This is your Project Root
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # This is your Project Root
 
 app.debug = True
 
@@ -23,25 +23,25 @@ config_data = json.load(f)
 f.close()
 
 
-
 def send_kiosk_request(request_type, data):
-    #print(data)
+    # print(data)
     url = "http://localhost:9090" + request_type
     r = requests.post(url, json=data)
     print(r.status_code)
 
-@app.route('/', methods=['GET','POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def m2_paypad_web_app():
 
     print(request.method)
-    #print(request.form.to_dict())
+    # print(request.form.to_dict())
     if request.method == 'POST':
         print('POST request')
         data = request.form.to_dict()
         print()
         pprint(data)
         print()
-        
+
         if data['request-type'] == 'sendPayRequest':
             print('Pay request')
             json_data = json.loads(data['payment-data'])
@@ -49,23 +49,27 @@ def m2_paypad_web_app():
             transaction_id = json_data['transaction_id']
             wallet_address = json_data['wallet_address']
             amount = json_data['amount']
-            json_data={"transaction_id": transaction_id, "wallet_address": wallet_address, "amount": amount}
-            
+            json_data = {
+                "transaction_id": transaction_id,
+                "wallet_address": wallet_address,
+                "amount": amount}
+
             send_kiosk_request('/payment-request', json_data)
-            
-        elif data['request-type'] == 'cancelPayRequest': 
+
+        elif data['request-type'] == 'cancelPayRequest':
             json_data = {'request-type': 'cancelPayRequest'}
-            send_kiosk_request('/clear-display', json_data) 
+            send_kiosk_request('/clear-display', json_data)
             pass
-            
-        elif data['request-type'] == 'getStatus':  
+
+        elif data['request-type'] == 'getStatus':
             json_data = {}
-            send_kiosk_request('/payment-status', json_data)         
+            send_kiosk_request('/payment-status', json_data)
             pass
 
     return render_template('index.html', config_data=config_data)
 
-@app.route('/save-config-data', methods=['GET','POST'])
+
+@app.route('/save-config-data', methods=['GET', 'POST'])
 def save_config_data():
     if request.method == 'POST':
         print('save')
@@ -77,8 +81,8 @@ def save_config_data():
 
     return render_template('index.html', config_data=config_data)
 
+
 # host='0.0.0.0' to make flask available on the local network
 if __name__ == "__main__":
-#    app.run(host='0.0.0.0', port=5000)
+    #    app.run(host='0.0.0.0', port=5000)
     app.run()
-
